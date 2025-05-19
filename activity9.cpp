91440837c0d11d0e4c7cd82bf91024040b319862
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-19 19:19:05 +0700

    add akses modifier dan member

--- activity9.cpp
+++ activity9.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 class Student {
-    
+    private:
+        static int studentTotal;
+    public:
+        string name;
+        string status;
+        int age;
+
 };
-   
